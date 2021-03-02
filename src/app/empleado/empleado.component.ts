914c0035faad9654e7573b23f2817ac06cefb90a
tomasArtaza77 <tomasartaza25@gmail.com>
2021-03-02 15:11:39 -0300

    Update

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -11,11 +11,36 @@ export class EmpleadoComponent implements OnInit {
   nombre = "Tomas";
   apellido = "Artaza";
   edad = 22;
-  empresa = "The Ripper";
+  //empresa = "The Ripper";
 
   /*getEdad(){
     return this.edad;
   }*/
+
+  habilitarBusq = false;
+  usuRegistrado = false;
+  textoDeRegistro = "No hay nadie registrado.";
+
+  getRegistoUsuario(){
+    this.usuRegistrado=false;
+  }
+
+  setUsuarioRegistrado(event:Event){
+    //alert("Usuario registrado.");
+    //this.textoDeRegistro = "Usuario registrado satisfactoriamente.";
+    //alert(event.target) //identifico el tipo de objeto.
+    
+    if( (<HTMLInputElement>event.target).value == "SI" ){ //localizo el objeto y encuentro el evento a trabajar.
+      this.textoDeRegistro = "Usuario registrado satisfactoriamente.";
+    } 
+    else{
+      this.textoDeRegistro = "No hay nadie registrado.";
+    }
+  }
+
+  buscaEmpresa(value:String){
+    
+  }
 
   constructor() { }
 
